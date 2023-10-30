5377717ac8b251d072478568f2ca633fe837d34c
rsolanoweb <rubensoljim@gmail.com>
2023-10-30 16:57:08 +0100

    build: update version

--- holidays_es/__init__.py
+++ holidays_es/__init__.py
@@ -4,6 +4,4 @@ from holidays_es.enums import Province, Scope
 from holidays_es.holidays import HolidaySpain
 
 
-__version__: str = "1.0.0"
-__author__: str = "Rubén Solano <rubensoljim@gmail.com>"
 __all__: Sequence[str] = ["HolidaySpain", "Province", "Scope"]
